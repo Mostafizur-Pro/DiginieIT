c5c9179c350db037f3f6d611e616c0d076561455
SabbirSami <smd71430@gmail.com>
2024-01-18 12:14:56 +0600

    navbar done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,24 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Welcome to Diginie iT ',
-  description: 'Diginie it',
-}
+    title: "Welcome to Diginie iT ",
+    description: "Diginie it",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={`bg-[#0F1110] ${inter.className}`}>
+                {children}
+            </body>
+        </html>
+    );
 }
