793ecf74afa6650d330e2f8adecd6cb8359c3c66
SabbirSami <smd71430@gmail.com>
2024-02-12 19:24:38 +0600

    fetch projects data

--- src/app/(withlayout)/projects/page.tsx
+++ src/app/(withlayout)/projects/page.tsx
@@ -1,10 +1,24 @@
+"use client";
 
+import useGetProjects from "@/components/Hook/useGetProjects";
 
 const Projects = () => {
+    const { projects, loading, error } = useGetProjects();
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    console.log(projects);
+
     return (
-        <div>
-            
-        </div>
+        <section className="container mx-auto lg:px-16 px-4 py-32">
+            Hello
+        </section>
     );
 };
 
